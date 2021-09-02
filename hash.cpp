31d4f8b279a0f65dcedfa1b2e351b72a2d0bcf96
zchengyun <1690132420@qq.com>
2021-09-03 01:22:56 +0800

    添加项目文件。

---
+++ hash.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <functional>
+#include <string>
+
+struct S {
+	std::string first_name;
+	std::string last_name;
+};
+
+namespace std {
+	template<>
+	class hash<S> {
+	public:
+		size_t operator()(const S& s) const
+		{
+			size_t h1 = std::hash<std::string>()(s.first_name);
+			size_t h2 = std::hash<std::string>()(s.last_name);//hash原理有空看
+			return h1 ^ (h2 << 1);
+		}
+	};
+}
+
+int main()
+{
+	S s;
+	s.first_name = "Bender";
+	s.last_name = "Rodriguez";
+	std::hash<S> hash_fn;
+
+	std::cout << "hash(s) = " << hash_fn(s) << "\n";
+	return 0;
+}
